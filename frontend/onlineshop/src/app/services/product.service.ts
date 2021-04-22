053c9c18e585ca9842f351dce8d661a351c844e1
rdm100 <robertode_marco@hotmail.com>
2021-04-22 16:53:54 +0100

    Implemented product.service.ts

--- frontend/onlineshop/src/app/services/product.service.ts
+++ frontend/onlineshop/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+       map(response => response._embedded.products)
+    );
+  }
+}
+//map the REST API JSON results to an array of Product objects in TypeScript.
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
