969cd5130da704541d378b8263aa28266db560af
rdm100 <robertode_marco@hotmail.com>
2021-04-27 18:04:13 +0100

    Updated product category menu component to subscribe to its service which I am about to write

--- frontend/onlineshop/src/app/components/product-category-menu/product-category-menu.component.ts
+++ frontend/onlineshop/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-category-menu',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-category-menu.component.css']
 })
 export class ProductCategoryMenuComponent implements OnInit {
+  productCategories: ProductCategory[];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+    this.productService.getProductCategories().subscribe(
+      data => {
+        console.log('Product categories = ' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    );
   }
 
 }
