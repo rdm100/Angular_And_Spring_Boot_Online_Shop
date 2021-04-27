4907af17375865125a9297903000422e57566de6
rdm100 <robertode_marco@hotmail.com>
2021-04-27 18:21:14 +0100

    Updated product service implementing the getProductCategories method used by the product category component which subscribes to it.

--- frontend/onlineshop/src/app/services/product.service.ts
+++ frontend/onlineshop/src/app/services/product.service.ts
@@ -3,12 +3,16 @@ import { HttpClient } from  '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -16,15 +20,28 @@ export class ProductService {
     // build spring boot URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
        map(response => response._embedded.products)
        //map the REST API JSON results to an array of Product objects in TypeScript products.
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
 }
 //unwraps the JSON from spring data rest _embedded entry 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+   productCategory: ProductCategory[];
   }
 }
