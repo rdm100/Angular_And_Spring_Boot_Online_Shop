952ce7aef685f0b76c5e7ca81c0b3a1de4d30cf6
rdm100 <robertode_marco@hotmail.com>
2021-05-04 16:44:05 +0100

    Re-factored addToCart method

--- frontend/onlineshop/src/app/services/cart.service.ts
+++ frontend/onlineshop/src/app/services/cart.service.ts
@@ -20,12 +20,7 @@ export class CartService {
 
     if (this.cartItems.length > 0) {
     // find the item in the cart based on item id 
-      for (const tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
     }
 
     // check if found
